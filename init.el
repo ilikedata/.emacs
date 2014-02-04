92d40270436598ba331ed1076afc4c8b164026b3
Tai <tai@projectsomewhere.com>
2014-02-05 00:15:02 +1100

    Added some JS stuff and also a kill whitespace function bound to M-?

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-(setq url-proxy-services '(("http" . "10.68.72.18:8888")))
+;(setq url-proxy-services '(("http" . "10.68.72.18:8888")))
 (require 'package)
 (add-to-list 'package-archives
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
@@ -35,3 +35,28 @@
    kept-new-versions 6
    kept-old-versions 2
    version-control t)       ; use versioned backups
+
+
+   ;;; adds symbols included by google closure to js2-additional-externs
+(add-hook 'js2-post-parse-callbacks
+	  (lambda ()
+	    (let ((buf (buffer-string))
+		  (index 0))
+	      (while (string-match "\\(goog\\.require\\|goog\\.provide\\)('\\([^'.]*\\)" buf index)
+		(setq index (+ 1 (match-end 0)))
+		(add-to-list 'js2-additional-externs (match-string 2 buf))))))
+
+(setq scss-compile-at-save nil)
+
+(defun whack-whitespace (arg)
+  "Delete all white space from point to the next word.  With prefix ARG
+    delete across newlines as well.  The only danger in this is that you
+    don't have to actually be at the end of a word to make it work.  It
+    skips over to the next whitespace and then whacks it all to the next
+    word."
+  (interactive "P")
+  (let ((regexp (if arg "[ \t\n]+" "[ \t]+")))
+    (re-search-forward regexp nil t)
+    (replace-match "" nil nil)))
+
+(global-set-key (kbd "M-?") (lambda () (interactive) (whack-whitespace 1)))
