c7091251e542b4d19f453b63d5e2fa41a47a547f
Tai Elliott <tai.elliott@news.com.au>
2014-02-04 11:39:50 +1100

    Save backups into ~/.saves

--- init.el
+++ init.el
@@ -24,6 +24,14 @@
 (show-paren-mode 1)
 (require 'paren)
 (set-face-background 'show-paren-match (face-background 'default))
-(set-face-foreground 'show-paren-match "#A00")
+(set-face-foreground 'show-paren-match "#F00")
 (set-face-attribute 'show-paren-match nil :weight 'extra-bold)
 
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.saves"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
